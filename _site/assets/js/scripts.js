8bf4fc80b9f681ec610d93d5953c3d697714150f
Ali Jarjis <ajarjis@hotmail.co.uk>
2018-09-10 23:06:34 +0100

    Better portfolio gallery.

---
+++ _site/assets/js/scripts.js
@@ -0,0 +1,10 @@
+"use strict";
+
+/**
+ * Navigates the browser to a provided URL.
+ * 
+ * @param {string} url  url of website to navigate to 
+ */
+function openLink(url) {
+    window.location.href = url;
+}

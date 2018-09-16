13596fb18724b4743b280f12c5e9898f0e2121a2
Ali Jarjis <ajarjis@hotmail.co.uk>
2018-09-16 15:38:20 +0100

    Animation added for scrolling, fixed footer and contact now set up.

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,6 +1,23 @@
 "use strict";
 
-// Initialises Bootstrap's tooltips component
 $(function () {
+
+    /**
+     * Provides a nice scroll animation to any element 
+     * containing the 'scroll_hidden' class
+     */
+    var coolScroll = function () {
+        $('.scroll_hidden').each(function () {
+            $(this).isInViewport()
+                .removeClass('scroll_hidden')
+                .addClass('scroll_visible');
+        });
+    }
+
+    // Cool scroll is run on start up and upon window scroll
+    coolScroll();
+    $(window).scroll(coolScroll);
+
+    // Initialises Bootstrap's tooltips component
     $('[data-toggle="tooltip"]').tooltip()
 })

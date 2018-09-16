3ff8b2ce77b82a0ddf4d8923e4cca206f2de9afb
Ali Jarjis <ajarjis@hotmail.co.uk>
2018-09-16 17:58:12 +0100

    Display success on message sent.

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,23 +1,32 @@
 "use strict";
 
+// Stuff to perform on page load
 $(function () {
-
-    /**
-     * Provides a nice scroll animation to any element 
-     * containing the 'scroll_hidden' class
-     */
-    var coolScroll = function () {
-        $('.scroll_hidden').each(function () {
-            $(this).isInViewport()
-                .removeClass('scroll_hidden')
-                .addClass('scroll_visible');
-        });
-    }
-
-    // Cool scroll is run on start up and upon window scroll
-    coolScroll();
-    $(window).scroll(coolScroll);
-
     // Initialises Bootstrap's tooltips component
-    $('[data-toggle="tooltip"]').tooltip()
+    $('[data-toggle="tooltip"]').tooltip();
+
+    // Cool scroll effect is run on start up and upon window scroll
+    setUpCoolScrollEffect();
+    $(window).scroll(setUpCoolScrollEffect);
 })
+
+/**
+ * Provides a nice scroll animation to any element 
+ * containing the 'scroll_hidden' class
+ */
+function setUpCoolScrollEffect() {
+    $('.scroll_hidden').each(function () {
+        $(this).isInViewport()
+            .removeClass('scroll_hidden')
+            .addClass('scroll_visible');
+    });
+}
+
+/**
+ * Displays a success message when user submits the contact form.
+ */
+function submitContactForm(event) {
+    // TODO: perform validation
+    event.preventDefault();
+    $('#contactSuccessModal').modal('show');
+}
